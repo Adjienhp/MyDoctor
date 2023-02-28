e3a3de21481c34499804c4d1d998a64ca12d845a
Adjienhp <adjienhp@gmail.com>
2023-02-28 12:57:55 +0700

    add global fonts and colors

---
+++ src/utils/colors/index.ts
@@ -0,0 +1,14 @@
+export const mainColor = {
+  white: '#FFFFFF',
+  dark: '#112340',
+};
+
+export const colors = {
+  background: {
+    white: mainColor.white,
+  },
+  font: {
+    white: mainColor.white,
+    dark: mainColor.dark,
+  },
+};
